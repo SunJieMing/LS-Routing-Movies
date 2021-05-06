800b08110a9a8771850f48eb23d5cceed87f2cc9
Dan Shaw <Dan@Dans-MacBook-Pro.local>
2021-05-06 00:15:11 -0400

    Components not rendering, although component tree visible from react dev tools - no compiler errors - turning in project

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from 'react';
+import { Route, Switch } from 'react-router-dom'
 import axios from 'axios';
+import Movie from './Movies/Movie'
+import MovieList from './Movies/MovieList'
+import SavedList from './Movies/SavedList'
 
-import SavedList from './Movies/SavedList';
 
 export default function App () {
   const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
@@ -11,7 +14,8 @@ export default function App () {
     const getMovies = () => {
       axios
         .get('http://localhost:5000/api/movies') // Study this endpoint with Postman
-        .then(response => {
+        .then(({data}) => {
+          setMovieList(data)
           // Study this response with a breakpoint or log statements
           // and set the response data as the 'movieList' slice of state
         })
@@ -28,9 +32,15 @@ export default function App () {
 
   return (
     <div>
-      <SavedList list={[ /* This is stretch */]} />
-
-      <div>Replace this Div with your Routes</div>
+      <Switch>
+        <SavedList list={[ /* This is stretch */]} />
+        <Route exact path='/'>
+          <MovieList movies={movieList} />
+        </Route>
+        <Route path='/movies/:id'>
+          <Movie movies={movieList} />
+        </Route> 
+      </Switch> 
     </div>
   );
 }
